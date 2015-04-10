7a6e1341530babef7f926485dbe47f8de9aa84e2
Armin Hutzler <armin.hutzler@student.tugraz.at>
2015-04-10 15:34:46 +0200

    [AHMS] Place Details

--- app/src/main/java/at/tugraz/beislrallye/MainActivity.java
+++ app/src/main/java/at/tugraz/beislrallye/MainActivity.java
@@ -1,28 +1,85 @@
 package at.tugraz.beislrallye;
 
+import android.content.IntentSender;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.Places;
 
 
-public class MainActivity extends ActionBarActivity{
+public class MainActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        Button button = (Button)findViewById(R.id.showPlacesButton);
+
+        button.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
+                                .getCurrentPlace(mGoogleApiClient, null);
+                        result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+                            @Override
+                            public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
+                                Place place = likelyPlaces.get(0).getPlace();
+                                getPlaceDetails(place);
+                                for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                                    Log.i("Likelihood", String.format("Place '%s' has likelihood: %g",
+                                            placeLikelihood.getPlace().getName(),
+                                            placeLikelihood.getLikelihood()));
+
+
+                                }
+                                //likelyPlaces.release();
+                            }
+                        });
+                    }
+                }
+        );
     }
 
+    private void getPlaceDetails(Place place) {
+
+    }
 
     @Override
     protected void onStart() {
         super.onStart();
+
+        mGoogleApiClient.connect();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+
+        mGoogleApiClient.disconnect();
     }
 
     @Override
@@ -45,5 +102,35 @@ public class MainActivity extends ActionBarActivity{
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i("Connection", "Connected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Called whenever the API client fails to connect.
+        Log.i("Error", "GoogleApiClient connection failed: " + result.toString());
+        if (!result.hasResolution()) {
+            // show the localized error dialog.
+            GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this, 0).show();
+            return;
+        }
+        // The failure has a resolution. Resolve it.
+        // Called typically when the app is not yet authorized, and an
+        // authorization
+        // dialog is displayed to the user.
+        try {
+            result.startResolutionForResult(this, 3);
+        } catch (IntentSender.SendIntentException e) {
+            Log.e("Resolution", "Exception while starting resolution activity", e);
+        }
     }
 }
